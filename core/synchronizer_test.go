be370e170e705de2810b937450c9cdab2e24e8b2
Amir Malka <amirm@armosec.io>
2023-10-30 12:30:06 +0200

    first test
    Signed-off-by: Amir Malka <amirm@armosec.io>

---
+++ core/synchronizer_test.go
@@ -0,0 +1,20 @@
+package core
+
+import (
+	"context"
+	"testing"
+
+	"github.com/kubescape/synchronizer/adapters"
+	"github.com/kubescape/synchronizer/domain"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSynchronizer_HandleSyncGetObject(t *testing.T) {
+	synchronizer := &Synchronizer{
+		adapter: adapters.NewMockAdapter(),
+		outPool: nil,
+	}
+
+	err := synchronizer.handleSyncGetObject(context.TODO(), domain.ClusterKindName{}, []byte("baseObject"))
+	assert.ErrorContains(t, err, "object not found")
+}
