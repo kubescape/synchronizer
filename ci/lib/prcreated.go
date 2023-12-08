5db2cebf837d7bb0cc2f0d453a706540e0273c5d
VaibhavMalik4187 <vaibhavmalik2018@gmail.com>
2023-12-08 16:49:36 +0530

    Added new functions in the lib package
    * Deleted the datastructures.go file as it was not being used.
    * Completed the CheckSecrets and EnvironmentTest functions in
      basic-tests.go
    * Moved several functions to semantically more correct locations.
    
    Signed-off-by: VaibhavMalik4187 <vaibhavmalik2018@gmail.com>

--- ci/lib/prcreated.go
+++ ci/lib/prcreated.go
@@ -2,13 +2,32 @@ package lib
 
 import (
 	"context"
+	"fmt"
+	"os"
 
 	"dagger.io/dagger"
 )
 
+// Run basic tests on PR
 func PrCreated(ctx context.Context, client *dagger.Client, src *dagger.Directory) error {
-	// run basic tests on PR
-	if err := BasicTests(ctx, client, src); err != nil {
+	basicTestsInputs := BasicTestsInputs{
+		GOVERSION:     os.Getenv("GO_VERSION"),
+		CGOENABLED:    os.Getenv("CGO_ENABLED") == "1",
+		BUILDPATH:     os.Getenv("BUILD_PATH"),
+		UNITTESTSPATH: os.Getenv("UNITTESTS_PATH"),
+	}
+
+	basicTestsSecrets := BasicTestsSecrets{
+		SNYK_TOKEN:          os.Getenv("SNYK_TOKEN"),
+		GITGUARDIAN_API_KEY: os.Getenv("GITGUARDIAN_API_KEY"),
+	}
+
+	fmt.Printf("GOVERSION: %s\n", basicTestsInputs.GOVERSION)
+	fmt.Printf("CGOENABLED: %t\n", basicTestsInputs.CGOENABLED)
+	fmt.Printf("BUILDPATH: %s\n", basicTestsInputs.BUILDPATH)
+	fmt.Printf("UNITTESTSPATH: %s\n", basicTestsInputs.UNITTESTSPATH)
+
+	if err := BasicTests(ctx, client, src, basicTestsInputs, basicTestsSecrets); err != nil {
 		return err
 	}
 
---
+++ ci/lib/utils.go
@@ -0,0 +1,34 @@
+package lib
+
+import (
+	"context"
+	"os"
+
+	"dagger.io/dagger"
+)
+
+// the platforms to build for and push in a multi-platform image
+var platforms = []dagger.Platform{
+	"linux/amd64",
+	"linux/arm64",
+}
+
+// the system tests to run on the newly built image
+var TestNames = []string{
+	"unit",
+}
+
+// the container registry for the multi-platform image
+const imageRepo = "quay.io/matthiasb_1/synchronizer"
+
+func InitDagger(ctx context.Context) (*dagger.Client, *dagger.Directory, error) {
+	client, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stdout))
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// get reference to the local project
+	// git checkout is performed by the calling CI system (e.g. GitHub Actions)
+	src := client.Host().Directory(".")
+	return client, src, nil
+}
