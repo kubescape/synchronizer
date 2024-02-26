b308a9ce18ee1821f53d28e24f2025b996644934
Amir Malka <amirm@armosec.io>
2024-02-26 16:31:17 +0200

    fix test
    Signed-off-by: Amir Malka <amirm@armosec.io>

--- tests/synchronizer_integration_test.go
+++ tests/synchronizer_integration_test.go
@@ -627,7 +627,7 @@ func initIntegrationTest(t *testing.T) *Test {
 	// synchronizer clients
 	createAndStartSynchronizerClient(t, cluster_1, clientConn1, synchronizerServer1)
 	createAndStartSynchronizerClient(t, cluster_2, clientConn2, synchronizerServer2)
-
+	time.Sleep(10 * time.Second)
 	return &Test{
 		ctx: ctx,
 		containers: map[string]testcontainers.Container{
